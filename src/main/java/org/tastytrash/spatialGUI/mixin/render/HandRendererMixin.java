37605708d53a8e29628ceb3278705ffe3023243a
tasty <notlukegwendling@gmail.com>
2026-09-15 16:11:57 -0400

    Updates for 26.3

--- src/main/java/org/tastytrash/spatialGUI/mixin/render/HandRendererMixin.java
+++ src/main/java/org/tastytrash/spatialGUI/mixin/render/HandRendererMixin.java
@@ -3,12 +3,12 @@ package org.tastytrash.spatialGUI.mixin.render;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.player.AbstractClientPlayer;
 //? if > 26.2 {
-/*import net.minecraft.client.renderer.FirstPersonHandsAndItemsRenderer;
+import net.minecraft.client.renderer.FirstPersonHandsAndItemsRenderer;
 import net.minecraft.client.renderer.state.level.FirstPersonHandsAndItemsRenderState;
 import net.minecraft.client.renderer.state.level.PlayerRenderState;
-*///? } else {
- import net.minecraft.client.renderer.ItemInHandRenderer;
-//? }
+//? } else {
+ /*import net.minecraft.client.renderer.ItemInHandRenderer;
+*///? }
 
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.world.InteractionHand;
@@ -22,7 +22,7 @@ import org.tastytrash.spatialGUI.SpatialGUI;
 import org.tastytrash.spatialGUI.client.SpatialGUIClient;
 
 //? if > 26.2 {
-/*@Mixin(FirstPersonHandsAndItemsRenderer.class)
+@Mixin(FirstPersonHandsAndItemsRenderer.class)
 public class HandRendererMixin {
     @Inject(method = "submitArmWithItem", at = @At("HEAD"), cancellable = true)
     private void spatialGUI$hideShield(PlayerRenderState playerState, FirstPersonHandsAndItemsRenderState state, float partialTicks, float xRot, InteractionHand hand, float attack, ItemStack itemStack, float inverseArmHeight, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int lightCoords, CallbackInfo ci) {
@@ -34,8 +34,8 @@ public class HandRendererMixin {
         }
     }
 }
-*///? } else {
-@Mixin(ItemInHandRenderer.class)
+//? } else {
+/*@Mixin(ItemInHandRenderer.class)
 public class HandRendererMixin {
 
     @Inject(method = "submitArmWithItem", at = @At("HEAD"), cancellable = true)
@@ -48,4 +48,4 @@ public class HandRendererMixin {
         }
     }
 }
-//? }
+*///? }
